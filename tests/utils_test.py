0921c79de3711920c054d05c7afccd602e85b119
rahulgautam21 <rahul.gautam21@gmail.com>
2022-10-09 20:37:54 +0000

    Automated autoyapf fixes

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -8,15 +8,16 @@ warnings.filterwarnings("ignore")
 
 
 class Tests(unittest.TestCase):
-	
-	# For testing the searchSong() method. Given a song name as input, should return the youtube link.
+
+    # For testing the searchSong() method. Given a song name as input, should return the youtube link.
     def test_searchSong(self):
         song_name = "Watermelon Sugar"
         actual_link = utils.searchSong(song_name)
         expected_link = "https://www.youtube.com/watch?v=E07s5ZYygMg"
         self.assertTrue(actual_link == expected_link)
 
-	# For testing the random_25() method. Checking if the list returned is of length 25.
+# For testing the random_25() method. Checking if the list returned is of length 25.
+
     def test_random_25(self):
         random_songs = utils.random_25()
         self.assertTrue(len(random_songs) == 25)
