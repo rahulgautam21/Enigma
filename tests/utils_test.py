85689f11afc1df68b96de04046903bb95019cd86
rahulgautam21 <rahul.gautam21@gmail.com>
2022-10-09 16:35:55 +0000

    Automated autoyapf fixes

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -6,13 +6,15 @@ from src import utils
 sys.path.append("../")
 warnings.filterwarnings("ignore")
 
-class Tests(unittest.TestCase):
-	def test_searchSong(self):
-		song_name = "Watermelon Sugar"
-		actual_link = utils.searchSong(song_name)
-		expected_link = "https://www.youtube.com/watch?v=E07s5ZYygMg"
-		self.assertTrue(actual_link == expected_link)
 
-	def test_random_ten(self):
-		random_songs = utils.random_ten()
-		self.assertTrue(len(random_songs) == 10)
+class Tests(unittest.TestCase):
+
+    def test_searchSong(self):
+        song_name = "Watermelon Sugar"
+        actual_link = utils.searchSong(song_name)
+        expected_link = "https://www.youtube.com/watch?v=E07s5ZYygMg"
+        self.assertTrue(actual_link == expected_link)
+
+    def test_random_ten(self):
+        random_songs = utils.random_ten()
+        self.assertTrue(len(random_songs) == 10)
