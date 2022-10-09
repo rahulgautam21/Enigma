47a2741020afedcf7099aef92b943110a0ef1aff
rahulgautam21 <rahul.gautam21@gmail.com>
2022-10-09 05:45:05 +0000

    Automated autoyapf fixes

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,11 @@ This file is responsible for all the helper functions that are used
 """
 from youtubesearchpython import VideosSearch
 from src.get_all import filtered_songs
-
 """
 This function seaches the song on youtube and returns the URL
 """
+
+
 def searchSong(name_song):
     videosSearch = VideosSearch(name_song, limit=1)
     result = videosSearch.result()
@@ -15,10 +16,11 @@ def searchSong(name_song):
 
 
 all_songs = filtered_songs()[["title", "artist", "top genre"]]
-
 """
 This function returns random 10 songs for generating the poll for the user
 """
+
+
 def random_ten():
     ten_random_songs = (all_songs.sample(
         frac=1).groupby('top genre').head(1)).sample(10)
