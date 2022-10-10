40545dd26f727e85e3d4789399274f06f15aa03b
ShreyaReddy17 <43646675+ShreyaReddy17@users.noreply.github.com>
2022-10-09 20:55:58 -0400

    Update songs_cog_test.py

--- tests/songs_cog_test.py
+++ tests/songs_cog_test.py
@@ -12,6 +12,28 @@ warnings.filterwarnings("ignore")
 @pytest.mark.asyncio
 class Test_Songs_Cog(unittest.TestCase):
 
+
     async def test_resume(self):
         result = await Songs.resume()
         assert result == "The bot was not playing anything before this. Use play command"
+
+
+    async def test_stop(self):
+        result = await Songs.stop()
+        assert result == "The bot is not playing anything at the moment."
+
+
+    async def test_play_song(self):
+        result = await Songs.play_song()
+        assert result == "The bot is not connected to a voice channel."
+    
+
+    async def test_handle_empty_queue(self):
+        result = await Songs.handle_empty_queue()
+        assert result == "No recommendations present. First generate recommendations using /poll"
+    
+
+    async def test_pause(self):
+        result = await Songs.pause()
+        assert result == "The bot is not playing anything at the moment."
+
